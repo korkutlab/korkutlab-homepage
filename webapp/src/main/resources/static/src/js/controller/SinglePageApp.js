b2f8c89838d6fea7a23ddeaeb9ac73f955c724c3
Onur Sumer <s.onur.sumer@gmail.com>
2017-02-15 20:14:39 -0500

    simplified page switch functions

---
+++ webapp/src/main/resources/static/src/js/controller/SinglePageApp.js
@@ -0,0 +1,92 @@
+/**
+ * @author Selcuk Onur Sumer
+ */
+function SinglePageApp(options)
+{
+    var _defaultOpts = {
+        pageLoaderDelay: -1,
+        appContent: "#app_content",
+        appTemplateId: "main_page",
+        pageContent: "#page_content",
+        pageLoader: "#page_loader",
+        // TODO move these into pages!
+        mainView: "#main_view",
+        mainContent: "#main_content",
+        pages: {
+            home: {
+                name: "#home",
+                template: "home_page"
+            }
+        }
+    };
+
+    // merge options with default options to use defaults for missing values
+    var _options = jQuery.extend(true, {}, _defaultOpts, options);
+
+    function switchContent(pageId, params)
+    {
+        // hide everything first
+        $(_options.pageContent).children().hide();
+
+        if (_options.pageLoaderDelay > 0)
+        {
+            // show the loader image before starting the transition
+            $(_options.pageLoader).show();
+
+            setTimeout(function() {
+                loadPage(pageId, params);
+
+                // hide the loader image after transition completed
+                $(_options.pageLoader).hide();
+            }, _options.pageLoaderDelay);
+        }
+        else
+        {
+            loadPage(pageId, params);
+        }
+    }
+
+    function loadPage(id, params)
+    {
+        // init section if not initialized yet
+        if (!$(_options.pages[id].name).length)
+        {
+            var templateFn = _.template($("#" + _options.pages[id].template).html());
+            $(_options.pageContent).append(templateFn(params));
+        }
+
+        $(_options.pages[id].name).show();
+    }
+
+    function init()
+    {
+        // init router
+        var routerOpts = {};
+
+        // default router options
+        _.each(_options.pages, function(page, id) {
+            var target = "/" + id;
+            routerOpts[target] = function() {
+                switchContent(id, page.params || {});
+            };
+        });
+
+        var router = new Router(routerOpts);
+
+        router.configure({notfound: function() {
+            // TODO switch to the not found page! (a static error page)
+            //switchContent(unknown);
+            $(_options.pageContent).children().hide();
+        }});
+
+
+        // init static content
+        var templateFn = _.template($("#" + _options.appTemplateId).html());
+        $(_options.appContent).append(templateFn());
+
+        // load home page content initially
+        router.init("/home");
+    }
+
+    this.init = init;
+}
--- webapp/src/main/resources/static/src/js/main.js
+++ webapp/src/main/resources/static/src/js/main.js
@@ -4,6 +4,34 @@
  * @author Selcuk Onur Sumer
  */
 $(document).ready(function() {
-        var hotspots = new App();
-        hotspots.init();
+    var app = new SinglePageApp({
+        pages: {
+            home: {
+                name: "#home",
+                template: "home_page"
+            },
+            korkut: {
+                name: "#korkut",
+                template: "korkut_page"
+            },
+            research: {
+                name: "#research",
+                template: "research_page"
+            },
+            opportunities: {
+                name: "#opportunities",
+                template: "opportunities_page"
+            },
+            publications: {
+                name: "#publications",
+                template: "publications_page"
+            },
+            contact: {
+                name: "#contact",
+                template: "contact_page"
+            }
+        }
+    });
+
+    app.init();
 });
